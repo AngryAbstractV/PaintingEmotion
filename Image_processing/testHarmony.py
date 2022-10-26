5fe46e73f8711d0edee130c2bcbb6de03abd5152
Lfickling <lettiefickling@gmail.com>
2022-10-26 13:42:49 -0600

    add test images and harmony test class

---
+++ Image_processing/testHarmony.py
@@ -0,0 +1,30 @@
+#test cases for image processing driver
+import unittest
+from harmony import calcHarmony
+import numpy as np
+
+testEmpty3DNumpyArray = np.zeros((1,1,1), dtype=np.int32)
+
+class TestPainting(unittest.TestCase):
+    def test_painting_initialization(self):
+        painting1 = Painting("test", "PaintingEmotion/ExamplePaintings/test.jpg")
+        self.assertEqual(painting1.getImage(), testEmpty3DNumpyArray)
+        self.assertEqual(painting1.getHSVImage(), testEmpty3DNumpyArray)
+        self.assertEqual(painting1.getPropertiesList(), [0]*6)
+
+    def test_painting_preprocessing(self):55
+        painting1 = Painting("test", "PaintingEmotion/ExamplePaintings/test.jpg")
+        painting1.preprocessing()
+        self.assertIsNot(painting1.getImage(), testEmpty3DNumpyArray)
+        self.assertIsNot(painting1.getHSVImage(), testEmpty3DNumpyArray)
+        self.assertEqual(painting1.getPropertiesList(), [0]*6)
+
+    def test_painting_get_properties(self):
+        painting1 = Painting("test", "PaintingEmotion/ExamplePaintings/test.jpg")
+        painting1.preprocessing()
+        painting1.calculateProperties()
+        self.assertNotEqual(painting1.getPropertiesList(), [0]*6)
+        print(painting1.getPropertiesList())
+
+if __name__=='__main__':
+    unittest.main()
