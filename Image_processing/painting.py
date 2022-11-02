1b3a494bacf0dd119cabdb1daf76c5fc2c446b02
SerenaWhite <whiteserenae@gmail.com>
2022-11-02 14:42:02 -0600

    Minor Linter Fixes

--- Image_processing/painting.py
+++ Image_processing/painting.py
@@ -1,11 +1,11 @@
 import cv2
 import numpy as np
-#from balance import calcBalance
-#from emphasis import calcEmphasis
+# from balance import calcBalance
+# from emphasis import calcEmphasis
 from harmony import calcHarmony
 from variety import calcVariety
-#from gradation import calcGradation
-#from movement import calcMovement
+# from gradation import calcGradation
+# from movement import calcMovement
 
 class Painting: 
 
@@ -18,16 +18,16 @@ class Painting:
         self.hsv_img = np.zeros((1,1,1), dtype=np.int32)
     
     def preprocessing(self):
-        #pre-processesing on image
-        
-        #input image
+        # pre-processesing on image
+
+        # input image
         self.img = cv2.imread(self.imageAddress, 1)
 
         self.hsv_img = cv2.cvtColor(self.img, cv2.COLOR_BGR2HSV)
-        #hsv_img is a 3d numpy array with the inner array having values:
-            #HUE = 0 - 180 (179?)
-            #SATURATION = 0 - 255
-            #VALUE = 0 - 255
+        # hsv_img is a 3d numpy array with the inner array having values:
+            # HUE = 0 - 180 (179?)
+            # SATURATION = 0 - 255
+            # VALUE = 0 - 255
 
         # Scale image, preserving aspect ratio
         pix_threshold = 500
@@ -58,28 +58,28 @@ class Painting:
         return
 
     def calculateProperties(self):
-        #call all feature algorithms and return a list of feature scores normalized from 0 to 1
+        # call all feature algorithms and return a list of feature scores normalized from 0 to 1
 
-        #TODO: remove once a feature is complete and integrated into driver
-        #self.properties_list = [0.5]*6
+        # TODO: remove once a feature is complete and integrated into driver
+        # self.properties_list = [0.5]*6
 
-        #call balance
-        #self.properties_list[0] = calcBalance(self.hsv_img)
+        # call balance
+        # self.properties_list[0] = calcBalance(self.hsv_img)
 
-        #call emphasis
-        #self.properties_list[1] = calcEmphasis(self.hsv_img)
-        
-        #call harmony
+        # call emphasis
+        # self.properties_list[1] = calcEmphasis(self.hsv_img)
+
+        # call harmony
         self.properties_list[2] = calcHarmony(self.hsv_img)
 
-        #call variety
+        # call variety
         self.properties_list[3] = calcVariety(self.hsv_img)
 
-        #call gradation
-        #self.properties_list[4] = calcGradation(self.hsv_img)
+        # call gradation
+        # self.properties_list[4] = calcGradation(self.hsv_img)
 
-        #call movement
-        #self.properties_list[5] = calcMovement(self.hsv_img)
+        # call movement
+        # self.properties_list[5] = calcMovement(self.hsv_img)
         print("properties calculations complete")
         return
 
